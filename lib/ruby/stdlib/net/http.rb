e4f450351487fdf842d2c290bbfa9511f949a55e
Mohamed Hafez <mohamed.m.m.hafez@gmail.com>
2021-01-11 11:22:04 -0800

    Replace Timeout.timeout in Net::HTTP#connect

--- lib/ruby/stdlib/net/http.rb
+++ lib/ruby/stdlib/net/http.rb
@@ -946,14 +946,14 @@ module Net   #:nodoc:
       end
 
       D "opening connection to #{conn_address}:#{conn_port}..."
-      s = Timeout.timeout(@open_timeout, Net::OpenTimeout) {
+      s =
         begin
-          TCPSocket.open(conn_address, conn_port, @local_host, @local_port)
+          Socket.tcp conn_address, conn_port, nil, nil, connect_timeout: @open_timeout
         rescue => e
           raise e, "Failed to open TCP connection to " +
             "#{conn_address}:#{conn_port} (#{e.message})"
         end
-      }
+
       s.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
       D "opened"
       if use_ssl?
